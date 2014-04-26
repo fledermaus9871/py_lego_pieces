43d4d1432d932197541b7974beb523e34abdf032
Andrew Yan <andrew.yan@outlook.com>
2014-04-26 08:04:02 -0500

    created methods to unzip and parse lego lxfml file

--- lego_lxf_parse.py
+++ lego_lxf_parse.py
@@ -1,11 +1,31 @@
+import zipfile
+
 from lxml import etree
 
 class ParseLegoLXF(object):
 	
-	def __init__(self, lxf_pathname):
+	def __init__(self, lxf_pathname, extract_path, lxfml_name='IMAGE100.LXFML'):
 		
 		self.lxf = lxf_pathname
+		self.extract = extract_path
+		self.lxfml = lxfml_name
 		
-	def unzip_lxf(object):
+	def unzip_lxf(self):
 		
+		with zipfile.ZipFile(self.lxf, 'r') as z:
+			z.extractall(self.extract)
+			
+	def parse_lxfml(self):
 		
+		self.unzip_lxf()
+		lxfml_path = '%s/%s' % (self.extract, self.lxfml)
+		xml_doc = etree.parse(lxfml_path)
+		print xml_doc
+
+if __name__ == '__main__':
+	
+	lxf_path = '/home/andrewyan/Desktop/tmp/bus_lf_variant_hybrid.lxf'
+	extract_dest = '/home/andrewyan/Desktop/tmp/bus_unzip'
+	
+	lxf = ParseLegoLXF(lxf_path, extract_dest)
+	lxf.parse_lxfml()
